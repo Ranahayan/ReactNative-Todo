910669f2749420863697e482f600d95ed8e1780d
Ranahayan <2019cs237@student.uet.edu.pk>
2022-11-10 19:55:23 +0500

    unkonwn updation of Todo file

--- app/screens/home/Todo.js
+++ app/screens/home/Todo.js
@@ -1,21 +1,33 @@
 import React from "react";
-import { TouchableOpacity, Text, StyleSheet } from "react-native";
+import { TouchableOpacity, Text, StyleSheet, View } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
 
 export default function Todo({ item, handleDeleteToDo }) {
   return (
-    <TouchableOpacity onPress={() => handleDeleteToDo(item.key)}>
-      <Text style={styles.item}>{item.text}</Text>
+    <TouchableOpacity >
+      <View style={styles.item}>
+        <AntDesign onPress={() => handleDeleteToDo(item.key)} name="delete" size={24} color="black" />
+        <Text style={styles.content}>{item.text}</Text>
+      </View>
     </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
   item: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+  },
+  content: {
+    flex: 1,
     borderWidth: 1,
     borderStyle: "dashed",
     borderColor: "blcak",
     padding: 16,
     marginVertical: 4,
     borderRadius: 10,
+    marginStart: 4,
   },
 });
